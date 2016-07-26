f9bcd737ee13be7db46ba0179b63ec9ae2a2bc8c
Nicolas Widart <n.widart@gmail.com>
2016-07-26 10:08:11 +0200

    Create an interface for normalize classes

---
+++ src/Contracts/Normalizer.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Nwidart\LaravelNormalizer\Contracts;
+
+interface Normalizer
+{
+    /**
+     * Normalize the given data
+     * @param array $data
+     * @return array
+     */
+    public function normalize(array $data);
+}
