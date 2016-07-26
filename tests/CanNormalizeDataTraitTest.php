46ad6e1f7fccb9d92fe372e359384e249ed7d98c
Nicolas Widart <n.widart@gmail.com>
2016-07-26 10:48:14 +0200

    Adding a test case for the data normalizer trait

---
+++ tests/CanNormalizeDataTraitTest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Nwidart\LaravelNormalizer;
+
+use Illuminate\Database\Eloquent\Model;
+use Nwidart\LaravelNormalizer\Stubs\CustomNormalizer;
+use Nwidart\LaravelNormalizer\Traits\CanNormalizeData;
+
+class CanNormalizeDataTraitTest extends \PHPUnit_Framework_TestCase
+{
+    /** @test */
+    public function it_returns_empty_array_if_no_normalizers_are_found()
+    {
+        $model = new class extends Model {
+            use CanNormalizeData;
+        };
+
+        $this->assertEquals([], $model->getNormalizers());
+    }
+
+    /** @test */
+    public function it_gets_all_normalizer_classes()
+    {
+        $model = $this->getStubbedModel();
+
+        $this->assertCount(1, $model->getNormalizers());
+    }
+
+    /** @test */
+    public function it_loops_over_normalizers()
+    {
+        $model = $this->getStubbedModel();
+
+        $data = $model->normalize(['name' => 'saymyname']);
+
+        $this->assertEquals('SAYMYNAME', $data['name']);
+    }
+
+    private function getStubbedModel()
+    {
+        return new class extends Model {
+            use CanNormalizeData;
+            protected $normalizers = [CustomNormalizer::class];
+        };
+    }
+}
---
+++ tests/Stubs/CustomNormalizer.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Nwidart\LaravelNormalizer\Stubs;
+
+use Nwidart\LaravelNormalizer\Contracts\Normalizer;
+
+final class CustomNormalizer implements Normalizer
+{
+    /**
+     * Normalize the given data
+     * @param array $data
+     * @return array
+     */
+    public function normalize(array $data)
+    {
+        if (array_key_exists('name', $data)) {
+            $data['name'] = strtoupper($data['name']);
+        }
+
+        return $data;
+    }
+}
